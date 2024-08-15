2f3f755fc80a20d948866cb7eab28ea583bdd75b
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-08-15 21:46:26 +0200

    Mock data

--- Appetizers/Appetizers/Appetizer.swift
+++ Appetizers/Appetizers/Appetizer.swift
@@ -14,3 +14,20 @@ struct Appetizer: Codable {
     let calories: Int
     let imageURL: String
 }
+
+struct AppetizerResponse {
+    let request: [Appetizer]
+}
+
+struct MockData {
+    static let sampleAppetizer = Appetizer(id: 001,
+                                           carbs: 99,
+                                           protein: 20,
+                                           price: 10.99,
+                                           description: "This is the description of an appetiser",
+                                           name: "Test appetiser",
+                                           calories: 312,
+                                           imageURL: "house")
+    
+    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer]
+}
