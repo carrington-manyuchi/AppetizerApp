1b7545a1463d1df7fef9ef0956c07b0aa8a1eca6
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-08-16 17:53:59 +0200

    AccountView

--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 
 struct AccountView: View {
+    
+    @State private var firstName = ""
+    @State private var lastName = ""
+    @State private var email = ""
+    @State private var birthDate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+
+    
     var body: some View {
         NavigationView {
-            Text("Account")
-                .navigationTitle("👨🏼 Account")
+            
+            Form {
+                Section {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lastName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled()
+                    DatePicker("Birthday", selection: $birthDate, displayedComponents: .date)
+                    
+                    Button(action: {
+                        print("save")
+                    }, label: {
+                        Text("Save Changes")
+                    })
+                    
+                } header: {
+                    Text("Personal Info")
+                }
+                
+                Section {
+                    Toggle(isOn: $extraNapkins, label: {
+                        Text("Extra Napkins")
+                    })
+                    Toggle(isOn: $frequentRefills, label: {
+                        Text("Frequent Refills")
+                    })
+                } header: {
+                    Text("requests")
+                }
+                .toggleStyle(SwitchToggleStyle(tint: .brandPrimary))
+
+                }
+            }
+             .navigationTitle("👨🏼 Account")
+            
         }
     }
-}
+
 
 
 #Preview {
